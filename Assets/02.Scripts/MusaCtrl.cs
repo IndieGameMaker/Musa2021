cd6e713000d9b74bfa93dbaea493743ebce89e96
Lee Jae Hyun <myleje@gmail.com>
2021-04-21 15:39:06 +0900

    .

--- Assets/02.Scripts/MusaCtrl.cs
+++ Assets/02.Scripts/MusaCtrl.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class MusaCtrl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Ray ray;
+    private RaycastHit hit;
+    private Camera camera;
+    private Transform tr;
+
+    public float damping = 10.0f;
+    public float moveSpeed = 5.0f;
+
+    private Vector3 movePos = Vector3.zero;
+
     void Start()
     {
-        
+        camera = Camera.main;
+        tr = GetComponent<Transform>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        ray = camera.ScreenPointToRay(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+        }        
     }
 }
